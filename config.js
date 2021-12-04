a8143f3ba0060d727a0ed77169b0b5b326204793
anirudh274 <anirudh3717@gmail.com>
2021-12-04 17:57:17 +0530

    2.2

--- config.js
+++ config.js
@@ -4,11 +4,12 @@
 
 
 //Container for all envs
-var environtments = {} // this will hold all of the diff envs and sub-keys,
+var environtments = {}; // this will hold all of the diff envs and sub-keys,
 
 // Staging (default) environment
 environtments.staging = {
-    'port' : 3000, //we can choose any number as port
+    'httpPort' : 3000, //we can choose any number as port
+    'httpsPort' : 3001,
     'envName' : 'staging'
 
 };
@@ -17,17 +18,18 @@ environtments.staging = {
 
 //Production environment
 environtments.production = {
-    'port' : 5000, //we can choose any number as port
+    'httpPort' : 6000, //we can choose any number as port
+    'httpsPort' : 6001,
     'envName' : 'production'
 
 };
 
-//Determine which  envi was passed as a CLA 
-var currentEnvironment = typeof(process.env.NODE_ENV) == 'sting' ? process.env.NODE_ENV.toLowercase() : ''; //we are using a loop where if the input is in diff format then the NODE_ENV will change it to string in lowercase or return just a normal string
+//Determine which  envi was passed as a CLA
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : ''; //we are using a loop where if the input is in diff format then the NODE_ENV will change it to string in lowercase or return just a normal string
 
 // Check that the current envi  is one of the environments above, if not, default to staging
 var environmentToExport = typeof(environtments[currentEnvironment]) == 'object' ? environtments[currentEnvironment] : environtments.staging; //we are using an ifelse
 
 
-//export the module 
-module.exports = environmentToExport // we dont want to export entire envs as we can see from CODE LINE 7
+//export the module
+module.exports = environmentToExport; // we dont want to export entire envs as we can see from CODE LINE 7
--- index.js
+++ index.js
@@ -4,96 +4,117 @@ primary rule for the API
 
 //Dependencies
 var http = require('http');
+var https = require('https');
 var url =  require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config')
+var config = require('./config.js');
+var fs = require('fs');
 
-// the server should  repsond to all requests with a string
-var server = http.createServer(function(req, res){
-
-    //get the url and parse it
-    var parsedUrl = url.parse(req.url, true);
-
-    //get path from the url
-    var path = parsedUrl.pathname;
-    var trimmedpath = path.replace(/^\/+|\/+$/g,'');
-
-    //get the query string as an object
-    var querystringobject = parsedUrl.query;
-
-    //get the http
-    var method = req.method.toLowerCase();
-
-    //get the headers as an object
-    var headers = req.headers;
-
-    //get the payload if any
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function(data){
-        buffer += decoder.write(data);
-    });
-    req.on('end', function(){
-        buffer += decoder.end();
-
-        //choose the handler this request should go to.If one is not found then use the Notfound handler
-        var choosenHandler = typeof(router[trimmedpath]) != 'undefined' ? router[trimmedpath] : handlers.notfound;
-
-        //construct the data object to send to the handler
-        var data = {
-
-            'trimmedpath' : trimmedpath,
-            'querystringobject' : querystringobject,
-            'method' : method,
-            'headers' : headers,
-            'payload' : buffer
-        };
-
-        //route the  request to the handler specified in the router
-        choosenHandler(data, function(statuscode,payload){
-
-            //use the status code called back by the handler, or default to 200
-            statuscode = typeof(statuscode) == 'number' ? statuscode : 200; //terniary operator
-
-            //use the payload called back by the handler, or default to an empty object
-            payload = typeof(payload) == 'object' ?  payload : {};
-
-            //convert teh payload to a string
-            var payloadstring = JSON.stringify(payload);
-
-            // Return the response
-            res.setHeader('content-type','application/json') //we are sending json and it should parse the response as if its json
-            res.writeHead(statuscode);  // we using built-in righthead function that comes on every response object received by the HTTP server to right the status code
-            res.end(payloadstring);
-
-            //log the request path
-            console.log('Returning this response: ',statuscode,payloadstring);
-
-
-
-        });
-
-
-    });
-
-
+// Instantiating the http server
+var httpServer = http.createServer(function(req, res){
+  unifiedServer(req,res);
 });
 
-
-// start the server, and have it listen on port 3000
-server.listen(3000, function(){
-console.log("the server listening at port 3000");
+// start the HTTP server, and from config.js file it will decide what should be showcased as per the user preference as it has port keys in config.js (staging, Production)
+httpServer.listen(config.httpPort, function(){
+console.log("the server listening at port "+config.httpPort); //the "context" says the port name, and the port key
 
 }
 );
 
+// Instantiating HTTPS server
+httpServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem'),
+}; //key and certificate are crux for HTTPS and we need to declare it here with OPENSSL
+var httpsServer = https.createServer(httpServerOptions,function(req, res){
+  unifiedServer(req,res);
+});
+
+// Start HTTPS server
+httpsServer.listen(config.httpsPort, function(){
+console.log("the server listening at port "+config.httpsPort); //the "context" says the port name, and the port key
+
+}
+);
+
+
+//All the server logic for both the http and https createServer
+var unifiedServer = function(req,res){
+
+      //get the url and parse it
+      var parsedUrl = url.parse(req.url, true);
+
+      //get path from the url
+      var path = parsedUrl.pathname;
+      var trimmedpath = path.replace(/^\/+|\/+$/g,'');
+
+      //get the query string as an object
+      var querystringobject = parsedUrl.query;
+
+      //get the http
+      var method = req.method.toLowerCase();
+
+      //get the headers as an object
+      var headers = req.headers;
+
+      //get the payload if any
+      var decoder = new StringDecoder('utf-8');
+      var buffer = '';
+      req.on('data', function(data){
+          buffer += decoder.write(data);
+      });
+      req.on('end', function(){
+          buffer += decoder.end();
+
+          //choose the handler this request should go to.If one is not found then use the Notfound handler
+          var choosenHandler = typeof(router[trimmedpath]) != 'undefined' ? router[trimmedpath] : handlers.notfound;
+
+          //construct the data object to send to the handler
+          var data = {
+
+              'trimmedpath' : trimmedpath,
+              'querystringobject' : querystringobject,
+              'method' : method,
+              'headers' : headers,
+              'payload' : buffer
+          };
+
+          //route the  request to the handler specified in the router
+          choosenHandler(data, function(statuscode,payload){
+
+              //use the status code called back by the handler, or default to 200
+              statuscode = typeof(statuscode) == 'number' ? statuscode : 200; //terniary operator
+
+              //use the payload called back by the handler, or default to an empty object
+              payload = typeof(payload) == 'object' ?  payload : {};
+
+              //convert teh payload to a string
+              var payloadstring = JSON.stringify(payload);
+
+              // Return the response
+              res.setHeader('content-type','application/json'); //we are sending json and it should parse the response as if its json
+              res.writeHead(statuscode);  // we using built-in righthead function that comes on every response object received by the HTTP server to right the status code
+              res.end(payloadstring);
+
+              //log the request path
+              console.log('Returning this response: ',statuscode,payloadstring);
+
+
+
+          });
+
+
+      });
+
+};
+
 //Define handlers
 var handlers = {};
 
-//sample handler
-handlers.sample = function(data, callback){
-    // Callback a http status code, and a payload and payload should be object(Cuz we want our API to work with JSON)
-    callback(406,{'name': 'my name is sample handler'});
+// ping handlers
+handlers.ping = function(data, callback){
+  callback(200);
 
 };
 
@@ -105,5 +126,5 @@ handlers.notfound = function(data,callback){
 
 //Define a request router
 var router  ={
-    'sample' : handlers.sample //handler should get called
+    'ping' : handlers.ping //handler should get called
 };
